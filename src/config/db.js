4124eb18b7fc2851c5c9ded35e29f46ebdc7e268
Japhet Shaba <67335641+japhoo@users.noreply.github.com>
2026-02-06 22:51:29 -0500

    Prisma

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,30 @@
-import { PrismaClient } from '@prisma/client'; // Adjust import path if using custom output
+// import { PrismaClient } from '@prisma/client'; // Adjust import path if using custom output
+// import { PrismaPg } from '@prisma/adapter-pg';
+// import 'dotenv/config'; // Ensure environment variables are loaded
+
+// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+// // const prisma = new PrismaClient({ adapter });
+// const prisma = new PrismaClient({ adapter },{
+//     log: process.env.NODE_ENV === "development" ? ["error","warn"] : ["error"]
+// });
+
+
+
+
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 import { PrismaPg } from '@prisma/adapter-pg';
-import 'dotenv/config'; // Ensure environment variables are loaded
+import 'dotenv/config';
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-// const prisma = new PrismaClient({ adapter });
-const prisma = new PrismaClient({ adapter },{
-    log: process.env.NODE_ENV === "development" ? ["error","warn"] : ["error"]
+
+const prisma = new PrismaClient({
+  adapter,
+  log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"]
 });
+
+
+
 
 
 
